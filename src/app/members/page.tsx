504372a24882db4e389fc049180e718daee46b8b
unknown <lavanyninja@gmail.com>
2024-06-04 13:17:37 +0530

    Init project and added navbar

---
+++ src/app/members/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Members() {
+  return (
+    <div>
+        <h3>This will be members page</h3>
+        <Link href='/'>Go back home</Link>
+    </div>
+  )
+}
