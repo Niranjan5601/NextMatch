7674ec8db0d52db58326be0d11f7644d222568a8
unknown <lavanyninja@gmail.com>
2024-06-05 13:51:46 +0530

    Fetching session and token details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,38 @@
+import { auth } from "@/auth";
 import { Button } from "@nextui-org/react";
+import { signOut } from "@/auth";
 import Link from "next/link";
 import { FaRegSmile } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <h1 className="text-3xl">Hello</h1>
-      <Button
-        color="primary"
-        variant="bordered"
-        as={Link}
-        href="/members"
-        startContent={<FaRegSmile size={20} />}
-      >
-        Click me
-      </Button>
+      <h3 className="text-2xl font-semibold">User session data: </h3>
+      {session ? (
+        <div>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <Button
+              color="primary"
+              variant="bordered"
+              type="submit"
+              startContent={<FaRegSmile size={20} />}
+            >
+              Sign out
+            </Button>
+          </form>
+        </div>
+      ) : (
+        <div>Not Signed In</div>
+      )}
     </div>
   );
 }
