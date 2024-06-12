9e039815abccfcf47ead2956b93a8146fa9241fd
Niranjan5601 <lavanyninja@gmail.com.com>
2024-06-12 17:55:35 +0530

    added message feature and notification sidebar

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import MessageSideBar from './MessageSideBar'
 
 export default function MessagesPage() {
   return (
-    <div>MessagesPage</div>
+    <div className='grid grid-cols-12 gap-5 h-[80vh] mt-10'>
+      <div className='col-span-2'>
+        <MessageSideBar/>
+      </div>
+      <div className='col-span-10'>
+        Message Table goes here
+      </div>
+    </div>
   )
 }
