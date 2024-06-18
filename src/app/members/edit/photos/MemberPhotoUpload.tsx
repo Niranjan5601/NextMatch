31a870773a350cec81dfef7d19154b3684d0e6fa
Niranjan5601 <lavanyninja@gmail.com.com>
2024-06-18 18:31:43 +0530

    minor fix

--- src/app/members/edit/photos/MemberPhotoUpload.tsx
+++ src/app/members/edit/photos/MemberPhotoUpload.tsx
@@ -8,6 +8,7 @@ import { toast } from 'react-toastify';
 
 export default function MemberPhotoUpload() {
     const router = useRouter();
+    
     const onAddImage = async (result: CloudinaryUploadWidgetResults) =>{
         if(result.info && typeof result.info ==='object'){
             await addImage(result.info.secure_url,result.info.public_id)
--- src/components/MemberPhotos.tsx
+++ src/components/MemberPhotos.tsx
@@ -50,16 +50,16 @@ export default function MemberPhotos({ photos, editing, mainImageUrl }: Props) {
 
         <div className="grid grid-cols-5 gap-3 p-5">
             {photos &&
-                photos.map((photo) => (
+                photos.map( photo => (
                     <div key={photo.id} className="relative">
                         <MemberImage photo={photo} />
                         {editing && (
                             <>
                                 <div onClick={() => onSetMain(photo)} className="absolute top-3 left-3 z-50">
-                                    <StarButton selected={photo.url === mainImageUrl} loading={loading.isLoading && loading.type === ' main' && loading.id === photo.id} />
+                                    <StarButton selected={photo.url === mainImageUrl} loading={loading.isLoading && loading.type === 'main' && loading.id === photo.id} />
                                 </div>
                                 <div onClick={() => onDelete(photo)} className="absolute top-3 right-3 z-50">
-                                    <DeleteButton loading={loading.isLoading && loading.type === ' delete' && loading.id === photo.id} />
+                                    <DeleteButton loading={loading.isLoading && loading.type === 'delete' && loading.id === photo.id} />
                                 </div>
                             </>
                         )
